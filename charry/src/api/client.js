3cb105fe38cb60d9c2d10475930fe0852c6c3552
Srim <deamoon0801@naver.com>
2020-12-02 22:58:44 +0900

    [ADD] <api/client.js> api

---
+++ charry/src/api/client.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const client = axios.create({
+  baseURL: process.env.VUE_APP_API_URL,
+  headers: {
+    "Content-Type": "application/json"
+  }
+});
+
+export const getClientAccessToken = axios.create({
+  baseURL: process.env.VUE_APP_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${localStorage.getItem("accessToken")}`
+  }
+});
