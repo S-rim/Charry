47f11a66389502f5a9055a4e6241ad99828b9d06
Srim <deamoon0801@naver.com>
2020-12-06 02:21:24 +0900

    [MODIFY] <components/Login/Login.vue> logo

--- charry/src/components/Login/Login.vue
+++ charry/src/components/Login/Login.vue
@@ -2,7 +2,7 @@
   <div class="user-container">
     <div class="user-inner">
       <header class="auth-header">
-        <img src="../../assets/img/Logo.png" />
+        <img v-bind:src="logo" />
         <h1>CHARRY</h1>
       </header>
       <p>다양한 사람들과 소통해보세요!</p>
@@ -19,7 +19,7 @@
             </div>
             <router-link to="/sign-up" class="link">아직 계정이 없으신가요?</router-link>
           </form>
-          <button class="userPage button" @click="onLogin">로그인</button>
+          <button class="button" @click="onLogin">로그인</button>
         </div>
       </div>
     </div>
@@ -27,11 +27,13 @@
 </template>
 
 <script>
+import { logo } from "@/assets/img";
 import "../../assets/style/authGlobal.scss";
 export default {
   name: "Login",
   data () {
       return {
+          logo,
           email : '',
           password : ''
       }
