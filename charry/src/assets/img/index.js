4fc131280b91f64b20e80e2d26e36b306ecae5b3
Srim <deamoon0801@naver.com>
2020-12-05 23:07:46 +0900

    [ADD] <assets/img/index.js> export image

---
+++ charry/src/assets/img/index.js
@@ -0,0 +1,8 @@
+export { default as exit } from "./exit.png";
+export { default as delete } from "./delete.png";
+export { default as list } from "./list.png";
+export { default as logo } from "./Logo.png";
+export { default as logout } from "./logout.png";
+export { default as plus } from "./plus.png";
+export { default as mock } from "./mock.png";
+export { default as search } from "./search.png";
