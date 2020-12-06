2f429e8515759bc05c21f8503a2b676bf6c6552c
Srim <deamoon0801@naver.com>
2020-12-07 01:10:52 +0900

    [ADD] AddRoom feature

--- charry/src/components/ChatRoom/AddRoom.vue
+++ charry/src/components/ChatRoom/AddRoom.vue
@@ -1,13 +1,42 @@
 <template>
-  <div></div>
+  <div class="chat-container">
+    <section>
+      <header>
+        <img v-bind:src="logo" />
+        <h1>CHARRY</h1>
+      </header>
+    </section>
+  </div>
 </template>
 
 <script>
+import { logo } from "@/assets/img";
 export default {
-
-}
+  data() {
+    return {
+      logo,
+    };
+  },
+  methods: {
+    onAddRoom() {},
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+$color: #b378ed;
+.chat-container {
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 2rem;
 
+    h1 {
+      color: $color;
+      font-size: 5.688rem;
+      margin: 0 0 0 0.625rem;
+    }
+  }
+}
 </style>
