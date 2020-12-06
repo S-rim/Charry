35d24831055f644bc26f021aa16450554070a52c
Srim <deamoon0801@naver.com>
2020-12-07 01:07:07 +0900

    [ADD] socket.io

--- charry/src/main.js
+++ charry/src/main.js
@@ -2,9 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "@/store";
+import io from "socket.io-client";
 import "@/assets/style/global.scss";
 Vue.config.productionTip = false;
 
+export const socket = io(process.env.VUE_APP_BASE_URL);
+Vue.prototype.$socket = socket;
 new Vue({
   render: (h) => h(App),
   router,
