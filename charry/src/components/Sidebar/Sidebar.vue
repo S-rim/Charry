8278dbde30f39d278811de81a19865a1b575c521
Srim <deamoon0801@naver.com>
2020-12-07 00:54:06 +0900

    [ADD] Sidebar feature

---
+++ charry/src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,34 @@
+<template>
+  <aside class="sideContainer">
+    <Search @onSearchRoom="onRoomView" />
+    <ChatLogView v-bind:searchdata="searchData" />
+  </aside>
+</template>
+
+<script>
+import Search from "../Search/Search";
+import ChatLogView from "./ChatLogView";
+export default {
+  components: { Search, ChatLogView },
+  data() {
+    return {
+      searchData: [],
+    };
+  },
+  methods: {
+    onRoomView(data) {
+      this.searchData.push(data);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.sideContainer {
+  width: 22%;
+  border-right: 1px solid #70707033;
+  padding-top: 20px;
+  height: 100%;
+  display: inline-block;
+}
+</style>
