4d497a1e56b889022dbc2f09282ee9fea61540aa
Srim <deamoon0801@naver.com>
2020-12-07 00:56:14 +0900

    [ADD] <components/Sidebar/index.js> export components

---
+++ charry/src/components/Sidebar/index.js
@@ -0,0 +1,5 @@
+export { default as ChatListItem } from "./ChatListItem.vue";
+export { default as ChatLogItem } from "./ChatLogItem.vue";
+export { default as RoomList } from "./RoomList.vue";
+export { default as Sidebar } from "./Sidebar.vue";
+export { default as ChatLogView } from "./ChatLogView.vue";
