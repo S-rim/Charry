0cb72aa1320891a72a349c0fb782bdf4bf561d54
Srim <deamoon0801@naver.com>
2020-12-07 01:09:16 +0900

    [ADD] <components/AddRoom/index.vue>

---
+++ charry/src/components/AddRoom/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <add-room />
+  </div>
+</template>
+
+<script>
+import { AddRoom } from "./index";
+export default {
+  components: { "add-room": AddRoom },
+};
+</script>
+
+<style></style>
