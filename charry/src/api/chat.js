9741d8ba6c030e4d2a8e9cc6325060f687861f1d
Srim <deamoon0801@naver.com>
2020-12-07 01:07:55 +0900

    [ADD] getRoomTitle api

--- charry/src/api/chat.js
+++ charry/src/api/chat.js
@@ -1,25 +1,29 @@
 import { getClientAccessToken } from "./client";
 
-export const createRoom = title => {
+export const createRoom = (title) => {
   return getClientAccessToken.post("/chat/room", { title });
 };
 
-export const roomSearch = title => {
+export const roomSearch = (title) => {
   return getClientAccessToken.get("/chat/room/search?title=" + title);
 };
 
-export const joinRoom = roomId => {
+export const joinRoom = (roomId) => {
   return getClientAccessToken.post("/chat/room/join", { roomId });
 };
 
 export const getRoom = () => {
-  return getClientAccessToken.get("chat/room");
+  return getClientAccessToken.get("/chat/room");
 };
 
-export const destoryRoom = roomId => {
-  return getClientAccessToken.delete("chat/room", { roomID });
+export const destoryRoom = (roomId) => {
+  return getClientAccessToken.delete(`/chat/room/${roomId}`);
 };
 
-export const leaveRoom = roomID => {
-  return getClientAccessToken.delete("chat/member", { roomID });
+export const leaveRoom = (roomId) => {
+  return getClientAccessToken.delete("/chat/member", { roomId });
+};
+
+export const getRoomTitle = (roomId) => {
+  return getClientAccessToken.get(`/chat/room/title/${roomId}`);
 };
