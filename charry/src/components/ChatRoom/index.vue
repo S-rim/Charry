37eb4862731444fdf745b7576b4f742b706d84e2
Srim <deamoon0801@naver.com>
2020-12-07 01:09:28 +0900

    [ADD] <components/ChatRoom/index.vue>

--- charry/src/components/ChatRoom/index.vue
+++ charry/src/components/ChatRoom/index.vue
@@ -1,13 +1,14 @@
 <template>
-  <div>chatroom</div>
+  <div>
+    <chat-room />
+  </div>
 </template>
 
 <script>
+import { ChatRoomView } from "./index";
 export default {
-
-}
+  components: { "chat-room": ChatRoomView },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
